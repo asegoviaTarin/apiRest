12dff89a7b9a03e382ef536ec70b8afeabb75888
Alberto Segovia <asegovia@finametrix.com>
2018-04-06 17:16:17 +0200

    first commit

--- src/api/index.js
+++ src/api/index.js
@@ -5,13 +5,55 @@ import facets from './facets';
 export default ({ config, db }) => {
 	let api = Router();
 
-	// mount the facets resource
-	api.use('/facets', facets({ config, db }));
-
 	// perhaps expose some API metadata at the root
-	api.get('/', (req, res) => {
-		res.json({ version });
-	});
+	api.post('/login', (req, res) => {
+    if(req.body.password === ' ') {
+      let body = {
+        id: req.body.username,
+        username: req.body.username,
+        firstName: req.body.username,
+        lastName: req.body.username,
+        token: 'fake-jwt-token'
+      };
+      res.json({ status: 200, body: body });
+    } else {
+      res.status(401).send('unauthorized');
+    }
+    
+  });
+  
+	api.get('/mockData', (req, res) => {
+      let body = [{
+        name: 'Chrome',
+        y: 61.41,
+        sliced: true,
+        selected: true,
+        likes: '358K'
+        }, {
+            name: 'Internet Explorer',
+            y: 11.84,
+            likes: '52K'
+        }, {
+            name: 'Firefox',
+            y: 10.85,
+            likes: '5K'
+        }, {
+            name: 'Edge',
+            y: 4.67,
+            likes: '56'
+
+        }, {
+            name: 'Safari',
+            y: 4.18,
+            likes: '105'
+
+        }, {
+            name: 'Other',
+            y: 7.05
+        }];
+      res.json({ status: 200, body: body });
+    });
+    
 
 	return api;
 }
